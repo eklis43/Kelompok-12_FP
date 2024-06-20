7dea9c0e470e8141c649af1c2d46982269171114
Ecclesiani <163414930+eklis43@users.noreply.github.com>
2024-06-20 20:58:36 +0700

    Delete dag/sql/silver directory

--- dag/sql/silver/product.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS silver.product;
-CREATE TABLE silver.product AS
-SELECT *,
-  CASE
-    WHEN price BETWEEN 0 AND 15 THEN 'Cheap'
-    WHEN price BETWEEN 16 AND 50 THEN 'Medium'
-    WHEN price > 50 THEN 'expensive'
-  END AS price_label
-FROM bronze.product;
